5c48ca33515a8e505a4e719db03b15d05cac390a
alokkkkkkk <adityanalin12@gmail.com>
2024-01-15 11:41:19 +0530

    128:Create save Post Feature

---
+++ src/test/java/myblog5/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package myblog5.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
